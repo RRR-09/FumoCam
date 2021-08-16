04a9aff937986197554eb4189f02fab05617185c
RusinR <79768142+RusinR@users.noreply.github.com>
2021-08-16 17:24:26 -0400

    Drastic structural changes

---
+++ src/tests.py
@@ -0,0 +1,37 @@
+from health import click_character_in_menu
+from injector import *
+
+
+def print_pos():
+    inject_lua_file("test_print_pos")  # For getting specific position in world
+
+
+def print_full_pos():
+    inject_lua_file("test_print_full_pos")  # For mapping new teleport entries with pos, rot, and cam rot
+
+
+def anti_gravity():
+    inject_lua_file("test_adjust_gravity", desired_gravity=0.0005)  # For getting to difficult places
+
+
+def silent_goto(player_name):
+    inject_lua_file("test_goto_player_silent", target=player_name.lower())  # Teleporting to a player for mapping
+
+
+def print_target_pos(player_name):
+    inject_lua_file("test_print_target_pos", target=player_name.lower())  # Getting players' positions for mapping
+
+
+def spectate_target(player_name):
+    inject_lua_file("test_spectate_player", target=player_name.lower())  # To see areas we get kicked if we teleport to
+
+
+def test_character_select(click_mouse=True):  # Character select OCR still needs work; guess coordinates and test
+    check_active()
+    sleep(1)
+    click_character_in_menu(click_mouse=click_mouse)
+
+
+if __name__ == "__main__":
+    pyautogui.FAILSAFE = False
+    test_character_select(click_mouse=True)
