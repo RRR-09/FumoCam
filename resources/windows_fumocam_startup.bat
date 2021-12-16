265ca0696b365cf8e99b57c6e8dc9b20a4be47c6
RRR-09 <79768142+RRR-09@users.noreply.github.com>
2021-12-16 00:26:19 -0500

    Change startup bat to use poetry

--- resources/windows_fumocam_startup.bat
+++ resources/windows_fumocam_startup.bat
@@ -1,6 +1,6 @@
 @echo off
-cd "%PROGRAMFILES(X86)%"\obs-studio\bin\64bit\
-start obs64.exe --minimize-to-tray --disable-updater --startstreaming
+::cd "%PROGRAMFILES(X86)%"\obs-studio\bin\64bit\
+::start obs64.exe --minimize-to-tray --disable-updater --startstreaming
 cd %USERPROFILE%\Desktop\FumoCam\src\
 echo [System Reboot Detected]> ..\output\main_process.txt
 echo Please wait. Initializing core systems. (0/3)> ..\output\main_status.txt
@@ -9,10 +9,10 @@ start /wait initialize_serial.py
 echo [System Reboot Detected]> ..\output\main_process.txt
 echo Please wait. Initializing core systems. (1/3)> ..\output\main_status.txt
 TIMEOUT /T 5
-start temps.py
+start poetry run python temps.py
 echo [System Reboot Detected]> ..\output\main_process.txt
 echo Please wait. Initializing core systems. (2/3)> ..\output\main_status.txt
 TIMEOUT /T 10
-start main.py
+start poetry run python main.py
 
 
