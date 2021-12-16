147f9bd5f28853282de01f097e84eeace44dd9ad
RRR-09 <79768142+RRR-09@users.noreply.github.com>
2021-12-16 09:33:12 -0500

    Uncomment OBS

--- resources/windows_fumocam_startup.bat
+++ resources/windows_fumocam_startup.bat
@@ -1,6 +1,6 @@
 @echo off
-::cd "%PROGRAMFILES(X86)%"\obs-studio\bin\64bit\
-::start obs64.exe --minimize-to-tray --disable-updater --startstreaming
+cd "%PROGRAMFILES(X86)%"\obs-studio\bin\64bit\
+start obs64.exe --minimize-to-tray --disable-updater --startstreaming
 cd %USERPROFILE%\Desktop\FumoCam\src\
 echo [System Reboot Detected]> ..\output\main_process.txt
 echo Please wait. Initializing core systems. (0/3)> ..\output\main_status.txt
